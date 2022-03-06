6b43550ed3ba890179e5fcb2a17ea768e4863c36
Patricia Faustino <patriciafaustinobr@gmail.com>
2022-03-06 00:31:28 -0300

    organizando o codigo

--- src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
+++ src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -1,22 +1,24 @@
 package br.com.alura.loja.testes;
 
+import br.com.alura.loja.dao.ProdutoDao;
 import br.com.alura.loja.modelo.Produto;
-
+import br.com.alura.loja.util.JPAUtil;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 import java.math.BigDecimal;
 
 public class CadastroDeProduto {
+
     public static void main(String[] args) {
         Produto celular = new Produto();
         celular.setName("Xiaomi Redmi");
         celular.setDescription("Melhor geração disponível");
         celular.setPrice(new BigDecimal("800"));
 
-        EntityManagerFactory factory = Persistence.createEntityManagerFactory("loja");
-        EntityManager entityManager = factory.createEntityManager();
+        EntityManager entityManager = JPAUtil.getEntityManager();
+
+        ProdutoDao dao = new ProdutoDao(entityManager);
+
         entityManager.getTransaction().begin();
         entityManager.persist(celular);
         entityManager.getTransaction().commit();
